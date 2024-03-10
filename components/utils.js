26bfaba5259dac0b76fba1fae9d978df5ac9c573
nikhilbhatia08 <bhatianikhil551@gmail.com>
2024-03-10 11:30:33 +0530

    first commit

---
+++ components/utils.js
@@ -0,0 +1,13 @@
+export const BASE_URL = 'http://192.168.0.248:3000'
+
+export const LOGIN_API = BASE_URL + '/User/Login'
+
+export const REGISTER_API = BASE_URL + '/User/Register'
+
+export const GET_TOP = BASE_URL + '/Course/get/top'
+
+export const GET_CATS = BASE_URL + '/Course/getCats'
+
+export const GET_COURSE = BASE_URL + '/Course/getCourse/'
+
+export const POST_FORUM = BASE_URL + '/Forum/CreateForum'
